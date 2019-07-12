1bca2cff75c3e3053e50aae739c365584ceff5cd
axel <axel.svanfeldt@gmail.com>
2019-07-12 23:03:02 +0200

    Moved useLocalStorageState to src/hooks/

---
+++ src/hooks/useLocalStorageState.js
@@ -0,0 +1,15 @@
+import {useState, useEffect} from 'react';
+
+const useLocalStorageState = (key, defaultVal) => {
+    const [value, setValue] = useState(
+        localStorage.getItem(key) || defaultVal
+    );
+
+    useEffect(() => {
+        localStorage.setItem(key, value);
+    });
+
+    return [value, setValue];
+}
+
+export default useLocalStorageState;
